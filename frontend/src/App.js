52f75929f603b39e03d42aa5ac87c25651ad3440
AyaShiab <shiab.ayah@gmail.com>
2023-03-06 21:35:01 +0300

    add component files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Project 4 </h1>
       </header>
+      <Routes>
+      <Route path="/" element={<Home />} /> 
+      </Routes>
     </div>
   );
 }

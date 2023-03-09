08cd28f4007e4d6a7dfca79bb367e430afed6293
AyaShiab <shiab.ayah@gmail.com>
2023-03-09 13:15:48 +0300

    add remove function from wishlist

--- frontend/src/components/WishList/index.js
+++ frontend/src/components/WishList/index.js
@@ -1,43 +1,64 @@
 import React, { useState, useEffect, useContext } from "react";
 import {
-    MDBCard,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
-    MDBCardImage,
-    MDBBtn
-  } from 'mdb-react-ui-kit';
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 import { CgDollar } from "react-icons/cg";
 
 const WishList = () => {
-    const [wishList, setwishList] = useState()
-    useEffect(()=>{
-        setwishList(JSON.parse(localStorage.getItem("fav")))
-         
-      },[])
-    console.log(wishList)
+  const [wishList, setwishList] = useState();
+  useEffect(() => {
+    setwishList(JSON.parse(localStorage.getItem("fav")));
+  }, []);
+  let fav = JSON.parse(localStorage.getItem("fav")) || [];
+  const deleteFromWish = (e) => {
+    console.log(e.target.id);
+    const id = e.target.id;
+    fav = fav.filter((product, i) => {
+      return product._id != id;
+    });
+    localStorage.setItem("fav", JSON.stringify(fav));
+    setwishList(JSON.parse(localStorage.getItem("fav")));
+  };
+
   return (
     <div>
-<h4 className="my-3">Wishlist</h4>
-<div className="row g-3">
-          {wishList && wishList.map((product, idx) => {
+      <h4 className="my-3">Wishlist</h4>
+      <div className="row g-3">
+        {wishList &&
+          wishList.map((product, idx) => {
             return (
-                <MDBCard style={{ maxWidth: "18rem", margin:"2rem",paddingTop:"1rem"}}>
-                <MDBCardImage src={product.image} position='top' alt='...'  />
+              <MDBCard
+                key={product._id}
+                style={{
+                  maxWidth: "18rem",
+                  margin: "2rem",
+                  paddingTop: "1rem",
+                }}
+              >
+                <MDBCardImage src={product.image} position="top" alt="..." />
                 <MDBCardBody>
-                  <MDBCardTitle style={{ textAlign: "center"}}>{product.productName}</MDBCardTitle>
+                  <MDBCardTitle style={{ textAlign: "center" }}>
+                    {product.productName}
+                  </MDBCardTitle>
                   <MDBCardText>
-                    <CgDollar/>{product.price}
+                    <CgDollar />
+                    {product.price}
                   </MDBCardText>
-                  <MDBBtn >Remove</MDBBtn>
+                  <MDBBtn id={product._id} onClick={deleteFromWish}>
+                    Remove
+                  </MDBBtn>
                 </MDBCardBody>
               </MDBCard>
             );
           })}
+      </div>
     </div>
-    </div>
-   
-  )
-}
+  );
+};
 
-export default WishList
+export default WishList;
